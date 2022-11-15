5d49a6549808ff614c17a9d8e87e2c0cde4ecfd7
Kinshuk <kinshuk2197kataria@gmail.com>
2022-11-16 01:19:07 +0530

    basic structure

---
+++ index.js
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt';
+
+class Block {
+  constructor() {}
+
+  getHash() {}
+}
+
+class Blockchain {
+  constructor() {}
+
+  addBlock(data) {}
+}
+
+const myFirstBlockChain = new Blockchain();
+
+myFirstBlockChain.addBlock({});
+myFirstBlockChain.addBlock({});
+myFirstBlockChain.addBlock({});

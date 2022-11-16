fed56a7c351ab03e2ba7d12738e3902856940669
Kinshuk <kinshuk2197kataria@gmail.com>
2022-11-16 16:00:35 +0530

    prototype-update

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
 import bcrypt from 'bcrypt';
 
 class Block {
-  constructor() {}
+  constructor(blockId, previousHash, data) {
+    this.blockId = blockId;
+    this.timeStamp = Date.now();
+    this.blockHash = this.getHash();
+    this.prevHash = previousHash;
+    this.data = data;
+  }
 
-  getHash() {}
+  getHash() {
+    return bcrypt.hashSync(
+      String(
+        this.blockId +
+          this.timeStamp +
+          this.blockHash +
+          this.previousHash +
+          JSON.stringify(this.data)
+      ),
+      10
+    );
+  }
 }
 
-class Blockchain {
-  constructor() {}
+class BlockChain {
+  constructor() {
+    this.chain = [];
+  }
 
-  addBlock(data) {}
+  addBlock(data) {
+    let blockId = this.chain.length;
+    let previousHash =
+      this.chain.length !== 0
+        ? this.chain[this.chain.length - 1].blockHash
+        : '';
+    let block = new Block(blockId, previousHash, data);
+    this.chain.push(block);
+  }
 }
 
-const myFirstBlockChain = new Blockchain();
+const MyFirstBlockChain = new BlockChain();
 
-myFirstBlockChain.addBlock({});
-myFirstBlockChain.addBlock({});
-myFirstBlockChain.addBlock({});
+MyFirstBlockChain.addBlock({
+  sender: 'Kinshuk',
+  reciever: 'Sachin',
+  amount: 3330000000
+});
+MyFirstBlockChain.addBlock({
+  sender: 'Satoshi',
+  reciever: 'Kataria',
+  amount: 3210000000
+});
+MyFirstBlockChain.addBlock({
+  sender: 'Kinshuk',
+  reciever: 'Bill',
+  amount: 3210000000
+});
+
+console.log(JSON.stringify(MyFirstBlockChain, null, 6));
